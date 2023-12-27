a4579bde7cfa3d7188098f9f31a6c5475fd09c4f
chvaldez10 <christianvaldez021@gmail.com>
2023-12-27 09:51:27 -0700

    modified esline config and added styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,37 @@
-// import React from "react";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Input = styled.input`
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
+  padding: 0.8rem 1.2rem;
+  box-shadow: var(--shadow-sm);
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onCLick={() => alert("Check out")}>Check out</Button>
+        <Input type="number" placeholder="number of guests" />
+        <Input type="number" placeholder="number of guests" />
+      </StyledApp>
+    </>
+  );
 }
